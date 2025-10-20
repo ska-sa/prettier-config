5d12b0a32128d87ea524919c8bc55a608d44d153
Zach <zsmith@sarao.ac.za>
2025-10-20 09:34:18 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  quoteProps: 'as-needed',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+}
